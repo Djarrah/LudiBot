fe1c1025352bd2e07a74df14385c80ed1822b20c
Djarrah <thecharx@gmail.com>
2020-05-18 14:23:43 +0200

    welcome message and minor changes

--- bot.py
+++ bot.py
@@ -10,33 +10,41 @@ TOKEN = os.environ["TOKEN"]
 bot = commands.Bot(command_prefix="/")
 CONSOLE_CHANNEL = "bot-console"
 WELCOME_ROLE = "Masquerader"
-GAME = "The Faded City RPG"
+GAME = "Umbreal"
+WELCOME_MSG = '''Hello and Welcome to the Ludi Personis Server!
+A place to organize, participate and discuss your favorite Role Playing Games, Board Games and also other stuff!
+There are few #rules here, and most fall under common sense, but check them out anyway.
+
+Looking for a game or for players to your game? Post in one of the channels in the Organization group.
+Do you want to discuss specific RPGs? Use the Game Discussions' channels.
+Do you simply want to chill? We got General Topics' channels for that.
+There are also channel sections for specific, server-organized games sometimes: check them out if you are interested.
+
+When you joined the server you also got the Masquerader role: this is used for important server-wide communications or events, but you can opt out of it if you want. Just type /masquerader in one of the server's channels (we have a #command-room to prevent cluttering) and if you change your mind, do the same to get your role back!
+Other roles you can get or remove yourself are:
+The Available role (/available) to signal you are available for future games and receive pings by organizers.
+The Organizer role (/organizer) to post in the organization channels.
+The Streamfan role (/streamfan) to be notified when a server-themed stream is happening (Yes, we have those sometimes, and you can stream too if you want!).
+
+Check the #faq channel for any doubt you could have, and if you still got them don't hesitate to ask in #general or even a Staff member.
+Now, go introduce yourself if you want, and don't forget to have fun!'''
 
 
 # Events
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(GAME))
-    print(f"{bot.user} connected to Discord in those servers:")
-    for guild in bot.guilds:
-        print(guild.name)
-        for channel in guild.channels:
-            if channel.name == CONSOLE_CHANNEL:
-                await channel.send("Bot ready!")
-                print("...with console!")
 
 @bot.event
 async def on_member_join(member):
     role = get(member.guild.roles, name=WELCOME_ROLE)
     await member.add_roles(role)
-    print(f"{member.name} joined the server {member.guild.name}")
-
+    await member.send(WELCOME_MSG)
 
 @bot.event
 async def on_member_remove(member):
     console = get(member.guild.channels, name=CONSOLE_CHANNEL)
     await console.send(f"{member.name} left the server :(")
-    print(f"{member.name} left the {member.guild.name} server")
 
 
 # Load Cogs
